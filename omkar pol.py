e03d82d7b5c8efb6d9d97cc403e6422198f7050e
omkarpol10 <118432636+omkarpol10@users.noreply.github.com>
2022-11-17 16:46:35 +0530

    Update omkar pol.py

--- omkar pol.py
+++ omkar pol.py
@@ -5,34 +5,34 @@ while True:
     if action=="break":
         break
     else:
-        InputData = []
-        InputData = action.split()
+        input_data = []
+        input_data = action.split()
         
         #to create account
-        if InputData[0]=="CREATE":
-            if InputData[1] in Data:
+        if input_data[0]=="CREATE":
+            if input_data[1] in Data:
                 print("Account already exist")
             else:
-                Data[InputData[1]]=[InputData[2],0]
+                Data[input_data[1]]=[input_data[2],0]
                 
         #to deposit amount
-        elif InputData[0]=="DEPOSIT":
-            if InputData[1] in Data:
-                (Data[InputData[1]][1])=(Data[InputData[1]][1])+int(InputData[2])
+        elif input_data[0]=="DEPOSIT":
+            if input_data[1] in Data:
+                (Data[input_data[1]][1])=(Data[input_data[1]][1])+int(input_data[2])
             else:
                 print("Account Does not exist")
         
         #to withdraw amount
-        elif InputData[0]=="WITHDRAW":
-            if InputData[1] in Data:
-                (Data[InputData[1]][1])=Data[InputData[1]][1]-int(InputData[2])
+        elif input_data[0]=="WITHDRAW":
+            if input_data[1] in Data:
+                (Data[input_data[1]][1])=Data[input_data[1]][1]-int(input_data[2])
             else:
                 print("Account Does not exist")
                 
         #to view balance
-        elif InputData[0]=="BALANCE":
-            if InputData[1] in Data:
-                print(Data[InputData[1]][0]+ " " +str(Data[InputData[1]][1]))
+        elif input_data[0]=="BALANCE":
+            if input_data[1] in Data:
+                print(Data[input_data[1]][0]+ " " +str(Data[input_data[1]][1]))
             else:
                 print("Account Does not exist")
 
